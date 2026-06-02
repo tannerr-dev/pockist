6813128b2426b4ab92c4104d33814b6a8eaf3195
Tanner Reyhons <tannerr.dev@protonmail.com>
2026-06-01 20:13:23 -0500

    inc sw v, deploy

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 console.log("Service Worker loaded.");
 
-const CACHE_NAME = "pockist-v20";
+const CACHE_NAME = "pockist-v21";
 
 self.addEventListener("install", function (event) {
 	event.waitUntil(
