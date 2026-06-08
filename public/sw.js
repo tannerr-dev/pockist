0d475c35b9d7f5bc7ccfc487bc022c543f9734d3
Tanner Reyhons <tannerr.dev@protonmail.com>
2026-06-07 19:11:57 -0500

    version in about page, pre release 0.1.25

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 console.log("Service Worker loaded.");
 
-const CACHE_NAME = "pockist-v24";
+const CACHE_NAME = "pockist-v0.1.25";
 
 self.addEventListener("install", function (event) {
 	event.waitUntil(
